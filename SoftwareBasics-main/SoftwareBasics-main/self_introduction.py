83e4a7a82c2573a53537277f3ef05eee1106cfe1
JUAN PARK <juan6park@gmail.com>
2026-02-19 18:11:44 +0900

    Delete SoftwareBasics-main directory

--- SoftwareBasics-main/SoftwareBasics-main/self_introduction.py
+++
@@ -1,8 +0,0 @@
-name = input("이름 :")
-major = input("전공 :")
-studentn = int(input("학번 :"))
-birth = input("생년월일 :")
-
-print("[자기소개]")
-print("숙명여대" , major , studentn , "학번" , name, "입니다.")
-print("제 생년월일은" , birth , "입니다.")
